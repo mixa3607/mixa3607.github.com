f7bf25d990e1398ff82d14dc568f6766e14cf453
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2026-04-17 20:23:57 +0500

    vllm: upd bench

--- docs/wiki/AMD_GFX906/vllm/benchmark/bench.run.sh
+++ docs/wiki/AMD_GFX906/vllm/benchmark/bench.run.sh
@@ -4,14 +4,16 @@
 export CONFIGS="$(cat bench-configs.yaml)"
 export VLLM_INST_HOST="127.0.0.1"
 export VLLM_INST_PORT="8000"
-export MODEL="$(curl http://$VLLM_INST_HOST:$VLLM_INST_PORT/v1/models | yq '.data[0].root' -r)"
+export MODELS="$(curl http://$VLLM_INST_HOST:$VLLM_INST_PORT/v1/models)"
+export MODEL_ROOT="$(echo "$MODELS" | yq '.data[0].root' -r)"
+export MODEL_NAME="$(echo "$MODELS" | yq '.data[0].id' -r)"
 for (( i=0; i<$(echo "$CONFIGS" | yq '.benchmarks | length'); i++ )); do
   BENCHMARK_NAME="$(echo "$CONFIGS" | yq ".benchmarks[$i].workload" -r)"
   BENCHMARK_ARGS="$(echo "$CONFIGS" | yq ".benchmarks[$i].args + \" \" + .baseArgs" -r | tr '\n' ' ')"
 
   echo "Run $BENCHMARK_NAME ($BENCHMARK_ARGS)"
   vllm bench serve $BENCHMARK_ARGS \
-    --host "$VLLM_INST_HOST" --model "$MODEL" --save-detailed --save-result \
+    --host "$VLLM_INST_HOST" --model "$MODEL_ROOT" --served-model-name "$MODEL_NAME" --save-detailed --save-result \
     --metadata metadata.workload="$BENCHMARK_NAME" metadata.image="$IMAGE_NAME"
   echo ""
 done
